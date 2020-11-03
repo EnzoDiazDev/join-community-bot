0388d1f673edbd6310160a18a3a46b717ad38ab9
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-03 20:01:08 +0100

    feat(Blopa): create blopa.js

---
+++ blopa.js
@@ -0,0 +1,32 @@
+/*
+    .slice - https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/slice
+    .slice returns a new array of the array to which the method is executed 
+*/
+
+/**
+ * set a dot at the end of the string.
+ * @params  {string} || {array} - str
+ * @return {string}, return a string dotted at the end
+ */
+
+function setDot(str) {
+
+  if (Array.isArray(str)) { 
+
+    const setToString = str.join(" "); //set the array to a string to show it
+    const Dot = setToString.slice(0, str.length - 1) + setToString.slice(1) + "."; //add a dot to the end of the string
+    return Dot;
+
+  } else if (typeof str === "string") { 
+
+    const setToArray = str.split(" "); //set the string to an array separating each word by spaces to be able to separate the final word
+    const Dot = str.slice(0, setToArray.length - 1) + str.slice(1) + "."; //add a dot to the end of the string
+    return Dot;
+
+  } else {
+
+    return "the str sent is not valid"; //case params are not string or array will be returned this
+
+  }
+
+};
