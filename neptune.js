24ec7acfdf246ff0d59153915bca7aacdb50dfa3
JoseMiguel2309 <josemiguelnanu@gmail.com>
2020-11-03 14:27:31 -0500

    NeptuneSek function

---
+++ neptune.js
@@ -0,0 +1,14 @@
+/*
+    String.concat(); permite unir dos strings para formar uno solo. 
+*/ 
+
+/**
+ * Concatena el nombre con un saludo.
+ * @param {string} name Un nombre.
+ * @returns {string} Retorna la bienvenida.
+ */
+
+function welcome(name){
+    const welcomeMessage = name.concat(", eres bienvenido");
+    return welcomeMessage;
+}
