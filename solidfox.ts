cae2db4df3f04754475adae1b89c7893ef6333de
Amodey <lamodey@asofarma.com>
2020-11-03 15:59:48 -0300

    Commit SolidFox - Participacion Community Bot

---
+++ solidfox.ts
@@ -0,0 +1,17 @@
+/*
+    Array.join() este método convierte una lista tipo Array en un string con los elementos separados por un separador . 
+*/
+
+
+/**
+ * Convierte un array en un string con sus elementos separados por un "separator".
+ * @function
+ * @param {Array} arr Un array con elementos de tipo string.
+ * @param {string} sep Un string que sirve como marca de separacion entre elementos
+ * @returns {string} Un string con los elementos del array separados por un separador 
+ */
+
+function convertToString( arr: string[], sep: string ){
+    var newString = arr.join(sep);
+    return newString;
+}
